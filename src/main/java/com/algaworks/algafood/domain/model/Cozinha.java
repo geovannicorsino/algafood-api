2ef7cf005b7ccba24f753f39df133c7ea3cbd3aa
Geovanni Corsino <geovannicorsino@gmail.com>
2021-01-31 19:59:51 -0300

    Projeto criado

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@JsonRootName("cozinha")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_cozinha")
+public class Cozinha {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+	
+}
