74185dd2e495185b36c6dfe9ec6aa4d4a3805dc9
Geovanni Corsino <geovannicorsino@gmail.com>
2021-02-01 09:15:37 -0300

    Usando named  query

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,19 +5,28 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
+@Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
 	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
-	
+
 	Optional<Restaurante> findFirstByNomeContaining(String nome);
-	
+
 	List<Restaurante> findTop2ByNomeContaining(String nome);
-	
+
 	int countByCozinhaId(Long cozinhaId);
-	
+
+	@Query("FROM Restaurante WHERE nome LIKE %:nome% AND cozinha.id = :id")
+	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+
+	List<Restaurante> consultarNome(String nome, @Param("id") Long cozinha);
+
 }
